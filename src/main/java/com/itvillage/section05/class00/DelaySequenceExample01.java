d5c3c7739b200e94f3afbeebe5493f80cfcd597e
hjs6877 <jungsik.hwang@codestates.com>
2023-09-30 14:43:54 +0900

    Add DelaySubscription and DelaySequence Example

---
+++ src/main/java/com/itvillage/section05/class00/DelaySequenceExample01.java
@@ -0,0 +1,23 @@
+package com.itvillage.section05.class00;
+
+import com.itvillage.utils.Logger;
+import com.itvillage.utils.TimeUtils;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+/**
+ * delaySequence 기본 개념 예제
+ *  - 구독은 즉시 이루어지지만 최초의 데이터 emit은 파라미터로 입력한 시간만큼 지연 시킨다.
+ */
+public class DelaySequenceExample01 {
+    public static void main(String[] args) {
+        Flux
+            .range(1, 10)
+            .doOnSubscribe(subscription -> Logger.doOnSubscribe())
+            .delaySequence(Duration.ofSeconds(2))
+            .subscribe(Logger::onNext);
+
+        TimeUtils.sleep(2500);
+    }
+}
