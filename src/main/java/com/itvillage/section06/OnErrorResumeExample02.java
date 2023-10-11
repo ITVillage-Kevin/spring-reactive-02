72a98df1fab3ad34574a0d44fbc73f3c982c3c2b
hjs6877 <jungsik.hwang@codestates.com>
2023-10-12 00:17:39 +0900

    .

--- src/main/java/com/itvillage/section06/OnErrorResumeExample02.java
+++ src/main/java/com/itvillage/section06/OnErrorResumeExample02.java
@@ -1,4 +1,4 @@
-package com.itvillage.section03.class07;
+package com.itvillage.section06;
 
 import com.itvillage.common.Member;
 import com.itvillage.utils.Logger;
@@ -8,40 +8,45 @@ import reactor.core.publisher.Mono;
  * then 활용 예제
  *  - 1 개의 task가 모두 끝났을 때, Complete Signal을 전달해서 추가 task를 수행하는 예제
  */
-public class thenExample03 {
+public class OnErrorResumeExample02 {
     public static void main(String[] args) {
         String email = "kevin@gmail.com";
 //        String email = "tom@gmail.com";
         verifyExistMember(email)
-            .then(
-                    saveMember(
-                            Member.builder()
-                                    .id(1L)
-                                    .email(email)
-                                    .name("Kevin")
-                                    .build()))
+                .onErrorResume(throwable -> {
+                    // 에러 처리 로직을 추가
+                    Logger.onError(throwable);
+                    return Mono.empty(); // 빈 Mono를 반환하여 이후 동작이 실행되지 않도록 함
+                })
+                .flatMap(notUse -> saveMember(
+                                        Member.builder()
+                                                .id(1L)
+                                                .email(email)
+                                                .name("Kevin")
+                                                .build())
+                )
             .subscribe(
                     createdMember -> Logger.onNext("created member", createdMember.getEmail()),
                     Logger::onError,
-                    () -> {}
+                    () -> Logger.onComplete()
             );
     }
 
-    private static Mono<Void> verifyExistMember(String email) {
+    private static Mono<Member> verifyExistMember(String email) {
         return selectFromMemberWhere(email)
+                .switchIfEmpty(Mono.just(Member.builder().build())) // saveMember()를 실행시킬 수 있도록 비어있는 member를 리턴한다.
                 .flatMap(foundMember -> {
-                    if (foundMember != null) {
+                    if (foundMember != null && foundMember.getEmail() != null) {
                         return Mono.error(new RuntimeException("Member exists"));
                     }
-                    return Mono.empty();
+                    return Mono.just(foundMember);
                 });
-
     }
 
     private static Mono<Member> selectFromMemberWhere(String email) {
         // 파라미터로 전달 받은 email로 DB에 조회한걸로 가정.
         Logger.info("# select from member where email=" + email);
-        Mono<Member> existMember = Mono.just(Member.builder().id(1L).email("kevin@gmail.com").name("Kevin").build());
+        Mono existMember = Mono.just(Member.builder().id(1L).email("kevin@gmail.com").name("Kevin").build());
 
         return email.equals("kevin@gmail.com") ? existMember : Mono.empty();
     }
