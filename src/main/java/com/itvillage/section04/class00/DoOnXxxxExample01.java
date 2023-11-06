2c179c76d7b664fe86691c1433c8b1df90ea63ab
jungsik-hwang <jungsik.hwang@codestates.com>
2023-11-06 23:46:02 +0900

    .

--- src/main/java/com/itvillage/section04/class00/DoOnXxxxExample01.java
+++ src/main/java/com/itvillage/section04/class00/DoOnXxxxExample01.java
@@ -12,14 +12,19 @@ public class DoOnXxxxExample01 {
     public static void main(String[] args) {
         Flux
             .range(1, 5)
-            .doFinally(signalType -> Logger.doFinally(signalType))
+            .doFinally(signalType -> Logger.info("doFinally() > range"))
             .doOnNext(data -> Logger.doOnNext("range", data))
-            .doOnRequest(n -> Logger.doOnRequest(n))
-            .doOnSubscribe(subscription -> Logger.doOnSubscribe())
-            .doFirst(() -> Logger.doFirst())
+            .doOnRequest(n -> Logger.info("doOnRequest > range: {}", 1))
+            .doOnSubscribe(subscription -> Logger.info("doOnSubscribe() > range"))
+            .doFirst(() -> Logger.info("doFirst() > range"))
+            .doOnComplete(() -> Logger.info("doOnComplete() > range"))
             .filter(num -> num % 2 == 1)
             .doOnNext(data -> Logger.doOnNext("filter", data))
-            .doOnComplete(Logger::doOnComplete)
+            .doOnRequest(n -> Logger.info("doOnRequest > filter: {}", 1))
+            .doOnSubscribe(subscription -> Logger.info("doOnSubscribe() > filter"))
+            .doFinally(signalType -> Logger.info("doFinally() > filter"))
+            .doOnComplete(() -> Logger.info("doOnComplete() > filter"))
+            .doFirst(() -> Logger.info("doFirst() > filter"))
             .subscribe(new BaseSubscriber<>() {
                 @Override
                 protected void hookOnSubscribe(Subscription subscription) {
