e4b6aa1c9dfddc61bf357b78dcff8a25ee6fae70
jungsik-hwang <jungsik.hwang@codestates.com>
2023-12-05 00:02:44 +0900

    .

--- src/main/java/com/itvillage/section07/class01/BufferExample01.java
+++ src/main/java/com/itvillage/section07/class01/BufferExample01.java
@@ -5,8 +5,8 @@ import reactor.core.publisher.Flux;
 
 /**
  * buffer 기본 개념 예제
- * - Upstream에서 emit되는 첫 번째 데이터부터 maxSize 숫자만큼의 데이터를 List 버퍼로 한번에 emit한다.
- * - 마지막 버퍼가 포함하는 데이터는 maxSize보다 작거나 같다.
+ * - Upstream에서 emit되는 데이터가 maxSize 숫자만큼 버퍼에 채워지면 버퍼를 비운다.
+ * - 버퍼에서 비워진 데이터는 List 형태로 한 번에 Downstream에 emit된다.
  */
 public class BufferExample01 {
     public static void main(String[] args) {
--- src/main/java/com/itvillage/section07/class01/BufferTimeoutExample01.java
+++ src/main/java/com/itvillage/section07/class01/BufferTimeoutExample01.java
@@ -7,9 +7,9 @@ import java.time.Duration;
 
 /**
  * bufferTimeout 기본 개념 예제
- *  - Upstream에서 emit되는 첫 번째 데이터부터 maxTime 내에 emit된 데이터를 List 버퍼로 한번에 emit한다.
- *  - maxTime 내에 버퍼에 담을 수 있는 최대 데이터 개수는 maxSize이다.
- *  - 마지막 버퍼가 포함하는 데이터는 maxSize보다 작거나 같다.
+ *  - Upstream에서 emit된 데이터가 버퍼에 채워질 때, maxTime에 도달하면 버퍼를 비운다.
+ *  - maxTime에 도달하기 전에 maxSize 만큼의 데이터가 버퍼에 채워지면 maxTime까지 기다리지 않고, 버퍼를 비운다.
+ *
  */
 public class BufferTimeoutExample01 {
     public static void main(String[] args) {
